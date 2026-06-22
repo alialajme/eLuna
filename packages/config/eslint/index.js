7af6e1ef7d621c5d08269546708f5bb0901dba8c
alialajme <ali.alajme@gmail.com>
2026-06-22 18:23:42 +0400

    chore: remove redundant ESLint parser/plugins, add .prettierignore

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,8 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-explicit-any": "warn",
